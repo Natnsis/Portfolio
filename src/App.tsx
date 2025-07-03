447dfb2aa8f1372c031cf54fb855bc07d043a2d0
nati <nsisay49@gmail.com>
2025-07-02 18:11:14 -0600

    dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,10 @@
-import About from "./components/About"
-import Experience from './components/Experience';
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import Skills from './components/Skills';
-import Testimonials from "./components/Testimonials";
-import Work from './components/Work';
-import Footer from './components/Footer';
-import CopyFooter from "./components/CopyFooter";
-
-
-const App = () => {
+import { ThemeProvider } from "@/components/theme-provider"
+import Page from "./Page"
+function App() {
   return (
-    <div className="pt-10 px-3 flex flex-col gap-y-20 bg-gray-100">
-      {/* home */}
-      <div className="bg-white p-3 ">
-          <Header/>
-          <Hero/>
-      </div>
-
-      {/* about */}
-      <div>
-        <About/>
-      </div>
-      {/* Skills */}
-      <div>
-        <Skills/>
-      </div>
-      {/* Experience */}
-      <div>
-        <Experience />
-      </div>
-      {/* Work */}
-      <div>
-        <Work/>
-      </div>
-      {/* Experience */}
-      <div>
-        <Testimonials/>
-      </div>   
-      {/* Footer    */}
-      <div>
-        <Footer/>
-      </div>
-      {/* copy rule */}
-      <div>
-        <CopyFooter/>
-      </div>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Page/>
+    </ThemeProvider>
   )
 }
 
---
+++ src/Page.tsx
@@ -0,0 +1,53 @@
+import About from "./components/About"
+import Experience from './components/Experience';
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Skills from './components/Skills';
+import Testimonials from "./components/Testimonials";
+import Work from './components/Work';
+import Footer from './components/Footer';
+import CopyFooter from "./components/CopyFooter";
+
+
+const Page = () => {
+  return (
+    <div className="pt-10 px-3 flex flex-col gap-y-20 bg-gray-100">
+      {/* home */}
+      <div className="bg-white p-3 ">
+          <Header/>
+          <Hero/>
+      </div>
+
+      {/* about */}
+      <div>
+        <About/>
+      </div>
+      {/* Skills */}
+      <div>
+        <Skills/>
+      </div>
+      {/* Experience */}
+      <div>
+        <Experience />
+      </div>
+      {/* Work */}
+      <div>
+        <Work/>
+      </div>
+      {/* Experience */}
+      <div>
+        <Testimonials/>
+      </div>
+      {/* Footer    */}
+      <div>
+        <Footer/>
+      </div>
+      {/* copy rule */}
+      <div>
+        <CopyFooter/>
+      </div>
+    </div>
+  )
+}
+
+export default Page
